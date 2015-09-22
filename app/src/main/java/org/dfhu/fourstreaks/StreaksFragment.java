a4f0792efb0d7cfcdda6e9afc5533ad5155d48b0
Victory <git@dfhu.org>
2015-09-22 19:40:37 -0400

    run streaks calculation in AsyncTask

--- app/src/main/java/org/dfhu/fourstreaks/StreaksFragment.java
+++ app/src/main/java/org/dfhu/fourstreaks/StreaksFragment.java
@@ -2,6 +2,7 @@ package org.dfhu.fourstreaks;
 
 import android.content.Context;
 import android.database.Cursor;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -24,79 +25,122 @@ public class StreaksFragment extends Fragment {
     private TextView curNP;
     private TextView curKET;
     private View rootView;
-    private Context mainActivity;
+    private MainActivity mainActivity;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         rootView = inflater.inflate(R.layout.streaks, container, false);
 
-        mainActivity = getActivity();
+        mainActivity = (MainActivity) getActivity();
 
         curNCH = (TextView) rootView.findViewById(R.id.curNCH);
         curSOC = (TextView) rootView.findViewById(R.id.curSOC);
         curNP = (TextView) rootView.findViewById(R.id.curNP);
         curKET = (TextView) rootView.findViewById(R.id.curKET);
 
-        setCurrentStreaks(); // FIXME: 9/21/15 probably should be in async task
+        setCurrentStreaks();
 
         return rootView;
     }
 
-    private void setCurrentStreaks() {
-        DaysEventSource source = new DaysEventSource(mainActivity);
-        Cursor cursor = source.getAllTopLevel();
+    private static class SetCurrentStreaksAsyncTask extends AsyncTask<MainActivity, Void, Integer[]> {
 
-        if (!cursor.moveToFirst()) {
-            return;
+        private MainActivity mainActivity;
+        private TextView curNCH;
+        private TextView curSOC;
+        private TextView curNP;
+        private TextView curKET;
+
+        private boolean isInStreak (Cursor cursor, boolean found, String columnName) {
+            return !found && cursor.getInt(cursor.getColumnIndexOrThrow(columnName)) == 1;
+
         }
 
-        int cntNCH = 0;
-        int cntSOC = 0;
-        int cntNP = 0;
-        int cntKET = 0;
-        boolean foundNCH = false;
-        boolean foundSOC = false;
-        boolean foundNP = false;
-        boolean foundKET = false;
-        do {
-            if (foundNCH && foundSOC && foundNP && foundKET) {
-                break; // found all current streaks
+        @Override
+        protected Integer[] doInBackground(MainActivity... mainActivities) {
+            mainActivity = mainActivities[0];
+
+            DaysEventSource source = new DaysEventSource(mainActivity);
+            Cursor cursor = source.getAllTopLevel();
+
+            if (!cursor.moveToFirst()) {
+                return null;
             }
 
-            if (isInStreak(cursor, foundNP, DaysEventHelper.C.flag_NCH)) {
-                cntNCH += 1;
-            } else {
-                foundNCH = true;
-            }
-            if (isInStreak(cursor, foundSOC, DaysEventHelper.C.flag_SOC)) {
-                cntSOC += 1;
-            } else {
-                foundSOC = true;
-            }
-            if (isInStreak(cursor, foundNP, DaysEventHelper.C.flag_NP)) {
-                cntNP += 1;
-            } else {
-                foundNP = true;
-            }
-            if (isInStreak(cursor, foundKET, DaysEventHelper.C.flag_KET)) {
-                cntKET += 1;
-            } else {
-                foundKET = true;
+            int cntNCH = 0;
+            int cntSOC = 0;
+            int cntNP = 0;
+            int cntKET = 0;
+            boolean foundNCH = false;
+            boolean foundSOC = false;
+            boolean foundNP = false;
+            boolean foundKET = false;
+            do {
+                if (foundNCH && foundSOC && foundNP && foundKET) {
+                    break; // found all current streaks
+                }
+
+                if (isInStreak(cursor, foundNP, DaysEventHelper.C.flag_NCH)) {
+                    cntNCH += 1;
+                } else {
+                    foundNCH = true;
+                }
+                if (isInStreak(cursor, foundSOC, DaysEventHelper.C.flag_SOC)) {
+                    cntSOC += 1;
+                } else {
+                    foundSOC = true;
+                }
+                if (isInStreak(cursor, foundNP, DaysEventHelper.C.flag_NP)) {
+                    cntNP += 1;
+                } else {
+                    foundNP = true;
+                }
+                if (isInStreak(cursor, foundKET, DaysEventHelper.C.flag_KET)) {
+                    cntKET += 1;
+                } else {
+                    foundKET = true;
+                }
+
+            } while (cursor.moveToNext());
+            cursor.close();
+
+            // FIXME: 9/22/15 this should be some sort of struct
+            return new Integer[]{cntNCH, cntSOC, cntNP, cntKET};
+        }
+
+        @Override
+        protected void onPostExecute(Integer[] integers) {
+            super.onPostExecute(integers);
+
+            if (integers == null) {
+                return;
             }
 
-        } while (cursor.moveToNext());
+            int cntNCH = integers[0];
+            int cntSOC = integers[1];
+            int cntNP = integers[2];
+            int cntKET = integers[3];
 
-        curNCH.setText(String.format("%d", cntNCH));
-        curSOC.setText(String.format("%d", cntSOC));
-        curNP.setText(String.format("%d", cntNP));
-        curKET.setText(String.format("%d", cntKET));
+            curNCH = (TextView) mainActivity.findViewById(R.id.curNCH);
+            curSOC = (TextView) mainActivity.findViewById(R.id.curSOC);
+            curNP = (TextView) mainActivity.findViewById(R.id.curNP);
+            curKET = (TextView) mainActivity.findViewById(R.id.curKET);
 
-        cursor.close();
+            curNCH.setText(String.format("%d", cntNCH));
+            curSOC.setText(String.format("%d", cntSOC));
+            curNP.setText(String.format("%d", cntNP));
+            curKET.setText(String.format("%d", cntKET));
+        }
     }
 
-    private boolean isInStreak (Cursor cursor, boolean found, String columnName) {
-        return !found && cursor.getInt(cursor.getColumnIndexOrThrow(columnName)) == 1;
+    private void setCurrentStreaks() {
+        new SetCurrentStreaksAsyncTask().execute(mainActivity);
+
+
+
 
     }
+
+
 }
