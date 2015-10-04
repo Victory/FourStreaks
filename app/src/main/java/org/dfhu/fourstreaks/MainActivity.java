bda50cdfaf18a2904c8c3320e9b503ba4b260c9f
Victory <git@dfhu.org>
2015-10-04 08:43:30 -0400

    remove action bar related callbacks as there is no actionbar

--- app/src/main/java/org/dfhu/fourstreaks/MainActivity.java
+++ app/src/main/java/org/dfhu/fourstreaks/MainActivity.java
@@ -1,5 +1,6 @@
 package org.dfhu.fourstreaks;
 
+import android.app.AlertDialog;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
@@ -31,28 +32,6 @@ public class MainActivity extends FragmentActivity implements EventCursorAdapter
 
         mViewPager = (ViewPager) findViewById(R.id.pager);
         mViewPager.setAdapter(mAppSectionsPagerAdapter);
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 
     public class AppSectionsPagerAdapter extends FragmentPagerAdapter {
